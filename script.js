c8e5ba23aae64eefd62a9401f78b94ecbc536951
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-25 00:55:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function updateElement(){
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve("Hello, world!");
+		},1000);
+	})
+}
+let output = document.getElementById("output");
+updateElement().then((text) => {
+	output.textContent = text;
+})
